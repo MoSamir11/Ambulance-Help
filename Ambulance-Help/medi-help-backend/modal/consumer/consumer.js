67ce05a2dc163dbe5d818e1ec05c4b69c86cc2f6
Mohammad Samir Ansari <mdsamiransari2000@gmail.com>
2022-03-06 21:51:06 +0530

    samir changes

---
+++ Ambulance-Help/medi-help-backend/modal/consumer/consumer.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const consumerSignupSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  email: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    default: null,
+  },
+  userType: {
+    type: String,
+    required: true,
+    default: null,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("consumer", consumerSignupSchema);
